367858db673f7fb0b159f2cdd702f879ded56f62
MD Fahim Abrar Asif <faasif929@gmail.com>
2025-09-28 14:20:32 +0600

    Update Footer

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -77,7 +77,7 @@ const Footer = () => {
 			</div>
 		</div>
 	</div>
-	<div className="py-6 text-sm text-center dark:text-gray-400">© 2022 Bro Tech. All rights reserved.</div>
+	<div className="py-6 text-sm text-center text-gray-400">© 2022 Bro Tech. All rights reserved.</div>
 </footer>
     );
 };
